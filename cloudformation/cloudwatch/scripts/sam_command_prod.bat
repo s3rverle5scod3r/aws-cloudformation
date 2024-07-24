34a911937056e31c4b1903ae06283f1ad2fa2f0e
s3rverle5scod3r <s3rverle5scod3r@hotmail.com>
2024-07-24 14:19:53 +0100

    Initial IaC file/folder structure commit

---
+++ cloudformation/cloudwatch/scripts/sam_command_prod.bat
@@ -0,0 +1,5 @@
+@echo off
+Rem This script runs the sam deploy command within the main script.
+Rem This prevents the script automatically closing, since 'sam deploy' produces a return code, which ends the executuon.
+
+sam deploy --template-file cloudwatch-main.yml --stack-name prod-cloudwatch-main-stack -- region eu-west-2 --s3-bucket sam-deploy --capabilities CAPABILITY_NAMED_IAM --no-fail-on-empty-changeset --parameter-overrides ParameterKey=Environment,ParameterValue=prod ParameterKey=Version,ParameterValue=%1 > deploy_output.txt
---
+++ cloudformation/secrets-manager/deploy/deploy_to_dev_stack.bat
@@ -0,0 +1,19 @@
+@echo off
+rem This script will automatically deploy the stack in a dev environment: use this for a feature branch.
+rem Parameters:
+rem %1: Version (String) : The major, minor and subminor version of this deployment.
+rem Execute usage: 'deploy_to_dev_stack.bat %1'
+rem Refer to GitLab/GitHub environment variables on CI configuration for current version.
+
+rem Change directory to root.
+cd..
+
+echo Deploying..
+cmd /C call scripts/sam_command_dev.bat %1
+echo Deployment completed.
+
+rem Change directory back to deploy for future deployments.
+cd deploy
+
+echo Script completed. Last updated %time%
+pause
